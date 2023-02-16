d48874ffa6e1a9ad25693e69f78c0f359ab9fb26
charlie <ljwcharlie@gmail.com>
2023-02-16 11:11:01 +0800

    set IGStory container

---
+++ src/pages/home/components/IGStory/index.tsx
@@ -0,0 +1,9 @@
+// import Item from "./Item";
+
+const IGStory: React.FC = () => {
+  return (
+    <div className="w-full h-[110px] box-border flex items-center overflow-x-auto overflow-y-hidden shadow-md no-scrollbar lg:my-8"></div>
+  );
+};
+
+export default IGStory;
