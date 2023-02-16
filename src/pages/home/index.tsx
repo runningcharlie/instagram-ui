5477d6c0fdbca85487da81ef5136eefdae4bed5d
charlie <ljwcharlie@gmail.com>
2023-02-16 11:03:36 +0800

    home page layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,18 @@
 import Header from "../../components/IGHeader";
+import IGContainer from "../../components/IGContainer";
 
 const Home: React.FC = () => {
   return (
     <>
       <Header />
-      <div className="flex lg:justify-center">
-        {/* left */}
-        <div className="w-full lg:w-[600px]"></div>
-        {/* right */}
-        <div className="hidden lg:block lg:w-[424px]"></div>
-      </div>
+      <IGContainer>
+        <div className="flex lg:justify-center">
+          {/* left */}
+          <div className="w-full lg:w-[600px]"></div>
+          {/* right */}
+          <div className="hidden lg:block lg:w-[424px]"></div>
+        </div>
+      </IGContainer>
     </>
   );
 };
