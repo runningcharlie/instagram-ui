4ea8ade04551f1e3eb5faf5a60ab2d2d51bc260d
charlie <ljwcharlie@gmail.com>
2023-02-16 11:36:17 +0800

    Use json to render story

--- src/pages/home/components/IGStory/index.tsx
+++ src/pages/home/components/IGStory/index.tsx
@@ -1,9 +1,15 @@
 import Item from "./Item";
+import db from "../../../../../db.json";
+
+const data = db.stories;
 
 const IGStory: React.FC = () => {
   return (
     <div className="w-full h-[110px] box-border flex items-center overflow-x-auto overflow-y-hidden shadow-md no-scrollbar lg:my-8">
-      <Item name="max" avatar="/images/avatars/a2.png" />
+      {data?.map((item) => {
+        const { id, name, avatar } = item;
+        return <Item key={id} name={name} avatar={avatar} />;
+      })}
     </div>
   );
 };
