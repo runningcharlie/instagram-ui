24c240e009dd935805278ecb9478c1f93635e3d7
charlie <ljwcharlie@gmail.com>
2023-02-16 11:01:49 +0800

    home page layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,12 @@ const Home: React.FC = () => {
   return (
     <>
       <Header />
+      <div className="flex lg:justify-center">
+        {/* left */}
+        <div className="w-full lg:w-[600px]"></div>
+        {/* right */}
+        <div className="hidden lg:block lg:w-[424px]"></div>
+      </div>
     </>
   );
 };
