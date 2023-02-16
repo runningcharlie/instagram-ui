6137dae3723fcab9b6360668e01f7d7982f76e01
charlie <ljwcharlie@gmail.com>
2023-02-16 09:50:38 +0800

    step of building router

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,13 @@ import Home from "./pages/home";
 import Following from "./pages/following";
 import NotFound from "./pages/notFound";
 
+/**
+ * 1. pages建立好。pages目录用于放页面级别的组件。
+ * 2. 写好router config
+ * 3. 引入BrowserRouter
+ * 4. 在App.tsx使用useRouters初始化路由
+ *  */ 
+
 const routes: RouteObject[] = [
   {
     path: "/",
