8b168110a4f9499725d22499924fef5a73038c07
charlie <ljwcharlie@gmail.com>
2023-02-16 11:17:37 +0800

    Create an IGStory item and use it

--- src/pages/home/components/IGStory/index.tsx
+++ src/pages/home/components/IGStory/index.tsx
@@ -1,8 +1,10 @@
-// import Item from "./Item";
+import Item from "./Item";
 
 const IGStory: React.FC = () => {
   return (
-    <div className="w-full h-[110px] box-border flex items-center overflow-x-auto overflow-y-hidden shadow-md no-scrollbar lg:my-8"></div>
+    <div className="w-full h-[110px] box-border flex items-center overflow-x-auto overflow-y-hidden shadow-md no-scrollbar lg:my-8">
+      <Item name="max" avatar="/images/avatars/a2.png" />
+    </div>
   );
 };
 
