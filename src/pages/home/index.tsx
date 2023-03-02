b54913baf3d658fee0a3e8c593208717f300c6b8
charlie <ljwcharlie@gmail.com>
2023-03-02 14:08:38 +0800

    获取数据然后渲染页面

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,95 +1,49 @@
 import Header from "../../components/IGHeader";
 import IGContainer from "../../components/IGContainer";
+import Loading from "../../components/Loading";
+
 import IGStory from "./components/IGStory";
 import IGPost from "./components/IGPost";
 import IGProfile from "./components/IGProfile";
+import { useGetIGPostsQuery } from "../../services/homeService";
 
 const IGPostList: React.FC = () => {
-  const data = [
-    {
-      id: 1,
-      location: "布魯斯前端",
-      account: "bruce_1234",
-      avatar: "/images/avatars/a1.png",
-      photo: "/images/posts/main1.png",
-      likes: 999,
-      description: "我的老天鵝！",
-      hashTags: "#鵝鵝",
-      createTime: "1 HOURS AGO",
-    },
-    {
-      id: 2,
-      location: "7-11你好門市",
-      account: "__0831_xxx__",
-      avatar: "/images/avatars/a2.png",
-      photo: "/images/posts/main2.png",
-      likes: 333,
-      description: "我的老天鵝！",
-      hashTags: "#鵝鵝",
-      createTime: "5 HOURS AGO",
-    },
-    {
-      id: 3,
-      location: "Taipei",
-      account: "gogogo_0214",
-      avatar: "/images/avatars/a3.png",
-      photo: "/images/posts/main3.png",
-      likes: 777,
-      description: "我的老天鵝！",
-      hashTags: "#鵝鵝",
-      createTime: "6 HOURS AGO",
-    },
-    {
-      id: 4,
-      location: "信義區",
-      account: "zoe_0000",
-      avatar: "/images/avatars/a4.png",
-      photo: "/images/posts/main4.png",
-      likes: 824,
-      description: "我的老天鵝！",
-      hashTags: "#鵝鵝",
-      createTime: "7 HOURS AGO",
-    },
-    {
-      id: 5,
-      location: "南港區",
-      account: "zoe_0000",
-      avatar: "/images/avatars/a5.png",
-      photo: "/images/posts/main5.png",
-      likes: 98,
-      description: "我的老天鵝！",
-      hashTags: "#鵝鵝",
-      createTime: "10 HOURS AGO",
-    },
-  ];
+  const { data, isLoading } = useGetIGPostsQuery("all");
   return (
     <>
-      {data?.map((item) => {
-        const {
-          id,
-          location,
-          account,
-          avatar,
-          photo,
-          likes,
-          description,
-          hashTags,
-          createTime,
-        } = item;
-        return (
-          <IGPost
-            location={location}
-            account={account}
-            avatar={avatar}
-            photo={photo}
-            likes={likes}
-            description={description}
-            hashTags={hashTags}
-            createTime={createTime}
-            key={id}
-          />
-        );
-      })}
+      {isLoading ? (
+        <div className="flex justify-center w-full">
+          <Loading />
+        </div>
+      ) : (
+        data?.map(
+          ({
+            id,
+            location,
+            account,
+            avatar,
+            photo,
+            likes,
+            description,
+            hashTags,
+            createTime,
+          }) => {
+            return (
+              <IGPost
+                location={location}
+                account={account}
+                avatar={avatar}
+                photo={photo}
+                likes={likes}
+                description={description}
+                hashTags={hashTags}
+                createTime={createTime}
+                key={id}
+              />
+            );
+          }
+        )
+      )}
     </>
   );
 };
