4f98e7e5b97e3426cecc73f0d3298657dea3d7c8
charlie <ljwcharlie@gmail.com>
2023-03-02 13:59:26 +0800

    query 服务器添加功能endpoints

--- src/services/homeService.ts
+++ src/services/homeService.ts
@@ -6,6 +6,18 @@ type IGStory = {
   avatar: string;
 };
 
+type IGPost = {
+  id: number;
+  location: string;
+  account: string;
+  avatar: string;
+  photo: string;
+  likes: number;
+  description: string;
+  hashTags: string;
+  createTime: string;
+};
+
 export const homeApi = createApi({
   reducerPath: "homeApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3004/" }),
@@ -18,7 +30,15 @@ export const homeApi = createApi({
         return "stories";
       },
     }),
+    getIGPosts: builder.query<IGPost[], number | "all">({
+      query: (id) => {
+        if (id !== "all") {
+          return `posts/${id}`;
+        }
+        return "posts";
+      },
+    }),
   }),
 });
 
-export const { useGetIGStoiesQuery } = homeApi;
+export const { useGetIGStoiesQuery, useGetIGPostsQuery } = homeApi;
