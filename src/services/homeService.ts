fb83d83958add120d05040b3f244591847efd67b
charlie <ljwcharlie@gmail.com>
2023-03-02 12:10:07 +0800

    创建服务器并编写第一个功能的endpoint

--- src/services/homeService.ts
+++ src/services/homeService.ts
@@ -1,13 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-type IGPost = {
+type IGStory = {
   id: number;
-  location: string;
-  account: string;
+  name: string;
   avatar: string;
-  photo: string;
-  likes: number;
-  description: string;
-  hashTags: string;
-  createTime: string;
 };
+
+export const homeApi = createApi({
+  reducerPath: "homeApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3004/" }),
+  endpoints: (builder) => ({
+    getIGStoies: builder.query<IGStory[], number | "all">({
+      query: (id) => {
+        if (id !== "all") {
+          return `stories/${id}`;
+        }
+        return "stories";
+      },
+    }),
+  }),
+});
