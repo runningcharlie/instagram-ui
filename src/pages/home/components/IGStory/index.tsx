c1651fed95ddce0d927510847a89009c7302f72a
charlie <ljwcharlie@gmail.com>
2023-03-02 12:39:47 +0800

    fix Loading 组件居中

--- src/pages/home/components/IGStory/index.tsx
+++ src/pages/home/components/IGStory/index.tsx
@@ -8,7 +8,9 @@ const IGStory: React.FC = () => {
   return (
     <div className="w-full h-[110px] box-border flex items-center overflow-x-auto overflow-y-hidden shadow-md no-scrollbar lg:my-8">
       {isLoading ? (
-        <Loading />
+        <div className="flex justify-center w-full">
+          <Loading />
+        </div>
       ) : (
         data?.map((item) => {
           const { id, name, avatar } = item;
