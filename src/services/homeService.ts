e17bb8817ed15e5aaee592cabd992258c3c1dd7e
charlie <ljwcharlie@gmail.com>
2023-03-02 12:06:35 +0800

    query - service 引入依赖和编写调试type

---
+++ src/services/homeService.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+type IGPost = {
+  id: number;
+  location: string;
+  account: string;
+  avatar: string;
+  photo: string;
+  likes: number;
+  description: string;
+  hashTags: string;
+  createTime: string;
+};
